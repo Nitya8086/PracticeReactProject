5d6946dbb30d9eb5956b2856b19e66a715d52f6e
Nitya Patel <nityapatel450@gmail.com>
2024-09-20 18:13:53 +0530

    first commit

--- CrudProject/src/components/UserList.jsx
+++ CrudProject/src/components/UserList.jsx
@@ -1,30 +1,29 @@
-import React from 'react';
+import React from "react";
 
-const UserList = ({ users, editUser,deleteUser }) => {
+const UserList = ({ users, editUser, deleteUser }) => {
   return (
     <div>
       <h2>User List</h2>
       <table>
         <thead>
           <tr>
-            <th>Name</th>
+            <th> User Name</th>
             <th>Email</th>
             <th>Phone</th>
-         
           </tr>
         </thead>
         <tbody>
-          {users.map((user,key )=> (
+          {users.map((user, key) => (
             <tr key={user.id}>
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>{user.phone}</td>
+
               <td>
                 <button onClick={() => editUser(user)}>Edit</button>
-                
               </td>
               <td>
-              <button onClick={() => deleteUser(user)}>Delete</button>
+                <button onClick={() => deleteUser(user)}>Delete</button>
               </td>
             </tr>
           ))}
